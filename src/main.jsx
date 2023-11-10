bee3808b0950bafc7aa80c8a77f5e6c747167865
Amy <amy@litdevs.org>
2023-11-10 17:20:42 +0200

    Router instrumentation

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Quarklight from './components/Quarklight.jsx'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {
+    createBrowserRouter,
+    createRoutesFromChildren, matchRoutes,
+    RouterProvider,
+    useLocation,
+    useNavigationType
+} from "react-router-dom";
 import "./style/root.css";
 import AssetRoot from "./components/asset/AssetRoot.jsx";
 import Client from "./components/Client.jsx";
@@ -14,6 +20,13 @@ Sentry.init({
     dsn: "https://c0236bc34da6926f2dbe59f139a02e5e@sentry.yggdrasil.cat/2",
     integrations: [
         new Sentry.BrowserTracing({
+            routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+                React.useEffect,
+                useLocation,
+                useNavigationType,
+                createRoutesFromChildren,
+                matchRoutes
+            ),
             // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
             tracePropagationTargets: ["localhost", /^https:\/\/[a-zA-Z0-9-.]+\.[a-zA-Z0-9]+\/v2.*/],
         }),
@@ -25,8 +38,9 @@ Sentry.init({
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
-
-const router = createBrowserRouter([
+const sentryCreateBrowserRouter =
+Sentry.wrapCreateBrowserRouter(createBrowserRouter);
+const router = sentryCreateBrowserRouter([
     {
         path: "/",
         element: <AssetRoot />,
