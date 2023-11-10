3b95eb6e1e6ab0f59dba21064e26f795961c06db
Amy <amy@litdevs.org>
2023-11-10 17:57:35 +0200

    Actually fix env

--- src/main.jsx
+++ src/main.jsx
@@ -32,13 +32,14 @@ Sentry.init({
         }),
         new Sentry.Replay(),
     ],
-    environment: import.meta.env.QV_ENV || "development",
+    environment: import.meta.env.VITE_APP_QV_ENV || "development",
     // Performance Monitoring
     tracesSampleRate: 1.0, // Capture 100% of the transactions
     // Session Replay
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
+console.log("Env", import.meta.env.VITE_APP_QV_ENV)
 const sentryCreateBrowserRouter =
 Sentry.wrapCreateBrowserRouter(createBrowserRouter);
 const router = sentryCreateBrowserRouter([
